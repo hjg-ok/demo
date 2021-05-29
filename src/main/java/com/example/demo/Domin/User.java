7df65ff2fea9a924284a92667490e4b276b64511
hjg-ok <870587689@qq.com>
2021-05-29 22:25:46 +0800

    SpringBoot git

---
+++ src/main/java/com/example/demo/Domin/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.Domin;
+
+
+public class User {
+    public String user;
+    public String pwd;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
---
+++ src/main/java/com/example/demo/TestController/ControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.TestController;
+
+import com.example.demo.Domin.User;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+
+import java.util.HashMap;
+
+@Controller
+@ResponseBody
+public class ControllerTest {
+    /**
+     *
+     */
+    @PostMapping(value = "/testPost")
+
+    public String test(@RequestBody String user) {
+//        HashMap<String,String> map= new HashMap();
+//        map.put("user",u.getUser());
+//        map.put("pwd",u.getPwd());
+//        System.out.println(u.getPwd());
+//        System.out.println(u.getUser());
+        System.out.println("user:" + user);
+//        System.out.println("pwd:"+pwd);
+        return "hello";
+    }
+}
